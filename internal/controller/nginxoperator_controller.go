3b6900ef852b2464ebdcd6875f8b7f5b5c776946
Rajkumar N <rajkumar.natarajan@gmail.com>
2026-06-24 09:43:50 +0530

    added/imported assets folder/package in Dockerfile/nginxoperator_controller.go

--- internal/controller/nginxoperator_controller.go
+++ internal/controller/nginxoperator_controller.go
@@ -27,6 +27,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	operatorv1alpha1 "github.com/rajnco/nginx-operator/api/v1alpha1"
+	"github.com/rajnco/nginx-operator/assets"
 )
 
 // NginxOperatorReconciler reconciles a NginxOperator object
